65512db9199c4617b2b23a63fe4cf10bee2695c0
Ajknight121 <aknig2@uic.edu>
2024-04-18 14:18:10 -0500

    Sizing fix

--- src/pages/Restaurant/components/Header/Header.jsx
+++ src/pages/Restaurant/components/Header/Header.jsx
@@ -2,15 +2,18 @@
 import React from 'react';
 import './Header.css';
 import shoppingCartImage from '../../assets/Shopping-Cart.png';
+import { Link } from 'react-router-dom';
 
 const Header = ({ totalQuantity, onCartClick }) => {
   return (
     <div className="header">
       <div className="header-content">
-        <div className="back-arrow">&lt;</div>
-        <h1>I Dream of Falafel</h1>
+        <Link to={"/explore"}>
+          <div className="back-arrow">&lt;</div>
+        </Link>
+        <h2>I Dream of Falafel</h2>
         <div className="cart-icon" onClick={onCartClick}>
-          <img src={shoppingCartImage} alt="Shopping Cart" />
+          <img src={shoppingCartImage} alt="Shopping Cart" width={"50px"} height={"50px"}/>
           {totalQuantity > 0 && <div className="cart-count">{totalQuantity}</div>}
         </div>
       </div>
