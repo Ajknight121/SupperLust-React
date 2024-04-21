ec55bdfae0447539e49d3ea5b84ca40a7d7c88a6
Ajknight121 <aknig2@uic.edu>
2024-04-21 13:27:03 -0500

    itemfocus handles meals

--- src/pages/pantry/Pantry.jsx
+++ src/pages/pantry/Pantry.jsx
@@ -1,9 +1,8 @@
-import { Link } from 'react-router-dom'
-import "./pantry.css"
+import { Link } from "react-router-dom";
+import "./pantry.css";
+import PantryItem from "./components/PantryItem";
 
 export default function Pantry() {
-
-
   return (
     <div>
       <div className="uppermost-nav">
@@ -13,24 +12,38 @@ export default function Pantry() {
         </div>
         <div className="shape-1" id="quick-bites-bttn">
           <img src="images/quick-bites-button.png" />
-          <Link to="/quickbites">Quick<br />Bites</Link>
+          <Link to="/quickbites">
+            Quick
+            <br />
+            Bites
+          </Link>
         </div>
         <span>Pantry</span>
         <img className="add-item" src="images/solid-brown-ellipse-3.png" />
-        <Link className="add-item" to="/additem">Add Item</Link>
+        <Link className="add-item" to="/additem">
+          Add Item
+        </Link>
         <img id="bottom-oval" src="images/solid-brown-ellipse-1.png" />
         <img id="center-oval" src="images/solid-brown-shaded-ellipse-2.png" />
       </div>
       <div className="lower">
-
         <img id="pantry-background" src="images/pantry-background.png" />
         <img id="shelf-first" src="images/shelf-1.png" />
+        <PantryItem
+          item={{
+            lifespan: "180 days",
+            quantity: "x12",
+            name: "Chicken Buillon Cubes",
+            imgFile: "images/buillion-chicken.png",
+          }}
+          urgent={true}
+        />
         <img id="shelf-second" src="images/shelf-2.png" />
         <img id="shelf-lower-1" src="images/shelf-3.png" />
         <img id="shelf-lower-2" src="images/shelf-3.png" />
       </div>
     </div>
-  )
+  );
 }
 
 // const Pantry = () => {
