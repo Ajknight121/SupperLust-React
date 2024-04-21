54c7cd57f42792619889eb797c8f1da203591dba
Zlrobbins9 <55512911+Zlrobbins9@users.noreply.github.com>
2024-04-21 16:36:34 -0500

    Added more items to bear5, moved IDOF and Bear5 to foods.js

--- src/Domain/SiteContext.jsx
+++ src/Domain/SiteContext.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { createContext, useState } from "react";
-import {BigRicoPizza} from "./foods.js"
+import {BigRicoPizza, IDOF, bear} from "./foods.js"
 
 // const CurrentDay = new Date("4/15/2024");
 
@@ -11,117 +11,7 @@ let ingredientExample = {
 };
 let foods = [ingredientExample, ingredientExample, ingredientExample];
 
-let falafel = {
-  id: 2,
-  image: "images/PlaceHolder.jpg",
-  name: "Falafel Pita Bread Bag",
-  rating: 2,
-  price: 4.75,
-  pickup: 19, // 24hr time same day
-  isBag: true,
-};
-
-let IDOFbags = [
-  {
-    id: 1,
-    image: "images/full-star.png",
-    name: "Shawarma Wrap Bag",
-    rating: 4,
-    price: "$4.75",
-    pickUpTime: "7:00 PM",
-    isBag: true,
-  },
-  {
-    id: 2,
-    image: "images/full-star.png",
-    name: "Falafel Pita Bread Bag",
-    rating: 2,
-    price: "$4.75",
-    pickUpTime: "7:00 PM",
-    isBag: true,
-  },
-  {
-    id: 3,
-    image: "images/full-star.png",
-    name: "Couscous Bag",
-    rating: 4,
-    price: "$2.00",
-    pickUpTime: "7:00 PM",
-    isBag: true,
-  },
-];
-
-const IDOFmeals = [
-  {
-    id: 4,
-    image: "images/hollow-star.png",
-    name: "Hummus Bag",
-    rating: 3,
-    price: "$2.00",
-    isBag: false,
-  },
-  {
-    id: 5,
-    image: "images/hollow-star.png",
-    name: "Baba Ganoush Bag",
-    rating: 4,
-    price: "$2.00",
-    isBag: false,
-  },
-  {
-    id: 6,
-    image: "images/hollow-star.png",
-    name: "Tabbouleh Bag",
-    rating: 5,
-    price: "$2.00",
-    isBag: false,
-  },
-];
-
-const IDOF = {
-  id: 1.1,
-  name: "I Dream of Falafel",
-  price: 30,
-  type: "Medditeraian",
-  distance: 1,
-  time: 20,
-  bags: IDOFbags,
-  meals: IDOFmeals,
-};
-
-const bear = {
-  id: 1.2,
-  name: "Bear 5 Burger",
-  price: 30,
-  type: "Burger",
-  distance: 0.3,
-  time: 10,
-  bags: [falafel],
-  meals: [
-    {
-      id: 4,
-      image: "images/PlaceHolder.jpg",
-      name: "Hummus Bag",
-      rating: 3,
-      price: "$2.00",
-      isBag: false,
-    },
-  ],
-};
-
 let restaurants = [
-  //   {
-  //     name: "I Dream of Queso Fundido",
-  //     price: 10,
-  //     type: "Mexican",
-  //     distance: 1,
-  //   },
-  //   {
-  //     name: "Pizza Time",
-  //     price: 10,
-  //     type: "Pizza",
-  //     distance: 2,
-  //   },
   IDOF,
   bear,
   BigRicoPizza,
--- src/Domain/foods.js
+++ src/Domain/foods.js
@@ -1,4 +1,144 @@
+
+
+
+
+//I dream of falafel
+let IDOFbags = [
+  {
+    id: 1,
+    image: "images/full-star.png",
+    name: "Shawarma Wrap Bag",
+    rating: 4,
+    price: "$4.75",
+    pickUpTime: "7:00 PM",
+    isBag: true,
+  },
+  {
+    id: 2,
+    image: "images/full-star.png",
+    name: "Falafel Pita Bread Bag",
+    rating: 2,
+    price: "$4.75",
+    pickUpTime: "7:00 PM",
+    isBag: true,
+  },
+  {
+    id: 3,
+    image: "images/full-star.png",
+    name: "Couscous Bag",
+    rating: 4,
+    price: "$2.00",
+    pickUpTime: "7:00 PM",
+    isBag: true,
+  },
+];
+
+const IDOFmeals = [
+  {
+    id: 4,
+    image: "images/hollow-star.png",
+    name: "Hummus Bag",
+    rating: 3,
+    price: "$2.00",
+    isBag: false,
+  },
+  {
+    id: 5,
+    image: "images/hollow-star.png",
+    name: "Baba Ganoush Bag",
+    rating: 4,
+    price: "$2.00",
+    isBag: false,
+  },
+  {
+    id: 6,
+    image: "images/hollow-star.png",
+    name: "Tabbouleh Bag",
+    rating: 5,
+    price: "$2.00",
+    isBag: false,
+  },
+];
+
+export const IDOF = {
+  id: 1.1,
+  name: "I Dream of Falafel",
+  price: 30,
+  type: "Medditeraian",
+  distance: 1,
+  time: 20,
+  bags: IDOFbags,
+  meals: IDOFmeals,
+};
+
+
+//Bear 5 burger//
+
+
+const BearBags = [
+  {
+    id: 1,
+    image: "images/PlaceHolder.jpg",
+    name: "Random Slices Bags",
+    rating: 3,
+    price: 4.75,
+    pickup: 19, // 24hr time same day
+    isBag: true,
+  },
+  {
+    id: 2,
+    image: "images/PlaceHolder.jpg",
+    name: "5 Bear Bag",
+    rating: 5,
+    price: 5.55,
+    pickup: 5, // 24hr time same day
+    isBag: true,
+  },
+];
+
+
+const BearMeals = [
+  {
+    id: 3,
+    image: "images/PlaceHolder.jpg",
+    name: "Yellow Chicken Pizza",
+    rating: 3,
+    price: "$9.00",
+    isBag: false,
+  },
+  {
+    id: 4,
+    image: "images/PlaceHolder.jpg",
+    name: "Mutton Chops",
+    rating: 3,
+    price: "$12.00",
+    isBag: false,
+  },
+  {
+    id: 5,
+    image: "images/PlaceHolder.jpg",
+    name: "Pirate Party Platter",
+    rating: 5,
+    price: "$35.00",
+    isBag: false,
+  },
+];
+
+export const bear = {
+  id: 1.2,
+  name: "Bear 5 Burger",
+  price: 30,
+  type: "Burger",
+  distance: 0.3,
+  time: 10,
+  bags: BearBags,
+  meals: BearMeals,
+};
+
+
 //Big Rico's Pizza
+
+
 const BigRicoBags = [
   {
     id: 1,
@@ -66,6 +206,23 @@ export const BigRicoPizza = {
   bags: BigRicoBags,
   meals: BigRicoMeals,
 };
+
+
+
+
+
+  //   {
+  //     name: "I Dream of Queso Fundido",
+  //     price: 10,
+  //     type: "Mexican",
+  //     distance: 1,
+  //   },
+  //   {
+  //     name: "Pizza Time",
+  //     price: 10,
+  //     type: "Pizza",
+  //     distance: 2,
+  //   },
 
 
 
